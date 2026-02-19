c3b10b533ea1185d99083168845bd739e18c3104
Rudi Jansen van Vuuren <rudi.jvvuuren@sybrin.com>
2026-02-19 17:13:27 +0200

    Removed faulty import

--- packages/vorplex/compiler/src/index.ts
+++ packages/vorplex/compiler/src/index.ts
@@ -1,9 +1,9 @@
 // **/*.ts
 export * from './compiler.util';
-export * from './script-analyzer.util';
-export * from './style-analyzer.util';
 export * from './module-loader/bundler';
 export * from './module-loader/module-loader.util';
+export * from './npm/jsdelivr/jsdelivr.util';
 export * from './npm/npm.util';
 export * from './npm/package-json.type';
-export * from './npm/jsdelivr/jsdelivr.util';
+export * from './script-analyzer.util';
+
