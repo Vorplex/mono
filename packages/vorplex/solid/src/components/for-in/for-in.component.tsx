cc21ad25bb0633c695ec912eca37c47f4ac8110d
Rudi Jansen van Vuuren <rudi.jvvuuren@sybrin.com>
2026-05-27 15:44:44 +0200

    Made for-in loop value an accessor

--- packages/vorplex/solid/src/components/for-in/for-in.component.tsx
+++ packages/vorplex/solid/src/components/for-in/for-in.component.tsx
@@ -2,7 +2,7 @@ import { For, type JSX } from 'solid-js';
 
 export interface ForInProps<TKey extends string, TValue> {
     each: Record<TKey, TValue>;
-    children: (value: TValue, key: TKey) => JSX.Element;
+    children: (value: () => TValue, key: TKey) => JSX.Element;
     fallback?: JSX.Element;
 }
 
@@ -12,7 +12,7 @@ export function ForIn<TKey extends string, TValue>(props: ForInProps<TKey, TValu
             each={Object.keys(props.each)}
             fallback={props.fallback}
         >
-            {(key) => props.children(props.each[key] as TValue, key as TKey)}
+            {(key) => props.children(() => props.each[key] as TValue, key as TKey)}
         </For>
     );
 }
