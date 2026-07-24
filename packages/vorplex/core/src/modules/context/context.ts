5f17769c74f662b5a42f53bdf7c744e2616ca503
Rudi Jansen van Vuuren <rudi.jvvuuren@sybrin.com>
2026-07-24 21:04:41 +0200

    Fixed context async dispose

--- packages/vorplex/core/src/modules/context/context.ts
+++ packages/vorplex/core/src/modules/context/context.ts
@@ -4,23 +4,29 @@ export class Context {
 
     public static create<T>(value?: T) {
         const stack = [value];
-        function context<R>(): T
-        function context<R>(value: T): Disposable & AsyncDisposable
+        function context(): T
+        function context(value: T): Disposable & AsyncDisposable
         function context<R>(value: T, callback: () => Awaitable<R>): Awaitable<R>
         function context<R>(...args: [value?: T, callback?: () => Awaitable<R>]): T | Awaitable<R> | Disposable | AsyncDisposable {
             if (args.length === 0) return stack[stack.length - 1];
             stack.push(args[0]);
             if (args.length === 1) {
+                let disposed: boolean;
+                const dispose = () => {
+                    if (disposed) return;
+                    disposed = true;
+                    stack.pop();
+                };
                 return {
-                    [Symbol.dispose]() { stack.pop(); },
-                    async [Symbol.asyncDispose]() { stack.pop(); }
+                    [Symbol.dispose]() { dispose(); },
+                    async [Symbol.asyncDispose]() { dispose(); }
                 };
             }
-            try {
-                return args[1]();
-            } finally {
-                stack.pop();
+            const result = args[1]();
+            if (result instanceof Promise) {
+                return result.finally(() => stack.pop());
             }
+            stack.pop();
         }
         return context;
     }
