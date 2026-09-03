594e6de5931b4dce56df27c62e105766b612e59b
Rudi Jansen van Vuuren <rudivuuren@gmail.com>
2026-09-03 16:22:39 +0200

    Fixed root level getter

--- packages/vorplex/solid/src/effects/use-store.ts
+++ packages/vorplex/solid/src/effects/use-store.ts
@@ -1,13 +1,17 @@
 import { $Value, Signal, SignalProxy, State } from '@vorplex/core';
-import { onCleanup } from 'solid-js';
+import { createSignal, onCleanup } from 'solid-js';
 import { createStore, reconcile } from 'solid-js/store';
 
 export function useStore<TState extends object>(state: State<TState>): SignalProxy<TState> {
     const [store, setStore] = createStore(state.value);
-    const subscription = state.subscribe(({ value }) => setStore(reconcile(value)));
+    const [root, setRoot] = createSignal(state.value);
+    const subscription = state.subscribe(({ value }) => {
+        setStore(reconcile(value));
+        setRoot(() => value);
+    });
     onCleanup(() => subscription.unsubscribe());
     return Signal.proxy(path => (((...args: any[]) => {
-        if (args.length === 0) return $Value.get(store, path);
+        if (args.length === 0) return path.length === 0 ? root() : $Value.get(store, path);
         state.set(path as any, args[0]);
         return args[0];
     }) as Signal));
